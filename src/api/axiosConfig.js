6407538c8315518356ffe4a5123779f39f8788d1
Bryan Rios <riosbryan456@gmail.com>
2026-02-21 22:21:57 -0500

    Actualizar URL del backend a Render

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080"; // URL base de tu backend
+const API_URL = "https://mi-proyecto-backend-relaxtotal.onrender.com"; // URL base de tu backend
 
 const apiClient = axios.create({
   baseURL: API_URL,
